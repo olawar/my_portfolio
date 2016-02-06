c9560a0ae29970ba0fc367689cd124b528200e90
olawar <warzecha.ola@gmail.com>
2016-02-06 21:18:00 +0100

    Created basic setup for the project with gitignore, readme and gulpfile

---
+++ README.md
@@ -0,0 +1,5 @@
+#My Portfolio Page
+
+##General Info
+
+Here you will find information about my skills, projects, professional experience and my contact data. 
---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var jshint = require('gulp-jshint');
+var stylish = require('jshint-stylish');
+var uglify = require('gulp-uglify');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var minifyCss = require('gulp-minify-css');
+
+
+gulp.task('lint', function() {
+  return gulp.src('./lib/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter(stylish));
+});
+
+gulp.task('scripts', function () {
+    gulp.src('src/js/**/*.js')
+        .pipe(concat('scripts.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('sass', function () {
+    gulp.src('src/sass/styles.scss')
+        .pipe(sass())
+        .pipe(autoprefixer())
+        .pipe(minifyCss())
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', ['lint', 'scripts', 'sass'], function () {
+    gulp.watch('src/js/**/*.js', ['lint', 'scripts']);
+    gulp.watch('src/sass/**/*.{sass,scss}', ['sass']);
+});
